80f4baa12637313a545845ae511f263e7deefbaf
PrasanthKeyan <prasanthkeyan@gmail.com>
2023-07-25 10:28:41 +0530

    first commit

---
+++ django_min_project/min_app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from.models import Course
+from.models import Technology
+from.models import Project_ideas
+
+# Register your models here.
+admin.site.register(Course)
+admin.site.register(Technology)
+admin.site.register(Project_ideas)
---
+++ django_min_project/min_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MinAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'min_app'
